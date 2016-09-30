42c4986c229521c9aea5f84645373521ee0b7667
Scott Page <scott.page@raise.com>
2016-09-30 18:51:03 -0500

    add hits

--- analyze.R
+++ analyze.R
@@ -3,3 +3,26 @@
 first_pass_summary <- function(data) {
   summary(train)
 }
+
+make_all_hist <- function(data) {
+  folder <- "histograms"
+  dir.create(folder, showWarnings = FALSE)
+  plots <- lapply(colnames(data), function(col) {
+    print(paste("Histogram for", col))
+    if(is.factor(data[[col]])){
+      p <- qplot(data[[col]],
+                 geom="bar",  
+                 main = paste("Barplot for", col), 
+                 xlab = col, 
+                 fill=I("blue"))
+    } else {
+      p <- qplot(data[[col]],
+                 geom="histogram",  
+                 main = paste("Histogram for", col), 
+                 xlab = col, 
+                 fill=I("blue"))
+    }
+    file_name <- paste0(folder, "/", col, ".png")
+    ggsave(file_name, plot = p)
+  })
+}
--- do.R
+++ do.R
@@ -2,7 +2,14 @@
 source("get.R")
 source("munge.R")
 source("analyze.R")
-library(dplyr)
+list.of.packages <- c('dplyr', 'randomForest', 'plotly', 'webshot', 'corrplot')
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) {
+  install.packages(new.packages)
+}
+for(package in list.of.packages){
+  library(package, character.only = TRUE)
+}
 
 train <- load_training_data()
 
@@ -12,5 +19,5 @@ observation_train <- nrow(train)
 #get the summary
 first_pass_summary(train)
 
-filled_missing_values_train <- fill_missing_values(train)
-
+filled_missing_values_train <- fill_missing_values(train) #calling the NA filling function in munge.R
+make_all_hist(filled_missing_values_train) #see histograms folder
